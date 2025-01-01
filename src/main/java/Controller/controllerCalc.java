b79b5465306fed6a74ff228b806a4ba2ca3da176
Masaya-windows <msy112091@gmail.com>
2025-01-01 23:15:45 +0900

    初めてのクリーンアーキテクチャ実装

--- src/main/java/Controller/controllerCalc.java
+++ src/main/java/Controller/controllerCalc.java
@@ -2,8 +2,18 @@ package Controller;
 
 import Service.serviceCalc;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api/controller")
 public class controllerCalc {
-    public static double[] execute(int originPrice, double[] ratios){
+
+    @GetMapping
+    public static double[] execute(
+            @RequestParam int originPrice,
+            @RequestParam double[] ratios){
         double[] result = serviceCalc.execute(originPrice, ratios);
         return result;
     }
